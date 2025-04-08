251be662e28e13223f9daa782f5448e3734dc59a
Matthew Fails <matthew.c.fails.com>
2025-04-07 21:57:55 -0400

    Booklisting form done

--- client/src/components/Booklisting.tsx
+++ client/src/components/Booklisting.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Booklisting from "../interfaces/Booklisting";
 
 
@@ -19,5 +20,52 @@ function BookListing(props: { listing?: Booklisting }) {
     );
   }
   
+  const BookListingForm = () => {
+    const [form, setForm] = useState({
+      title: '',
+      author: '',
+      genre: '',
+      description: '',
+      city: ''
+    });
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      console.log("Book Listing Submitted:", form);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title: <input name="title" value={form.title} onChange={handleChange} /></label>
+        </div>
+        <div>
+          <label>Author: <input name="author" value={form.author} onChange={handleChange} /></label>
+        </div>
+        <div>
+          <label>Genre: <input name="genre" value={form.genre} onChange={handleChange} /></label>
+        </div>
+        <div>
+          <label>Description: <textarea name="description" value={form.description} onChange={handleChange} /></label>
+        </div>
+        <div>
+          <label>City: <input name="city" value={form.city} onChange={handleChange} /></label>
+        </div>
+        <button type="submit">Create Listing</button>
+      </form>
+    );
+  };
+  
   export default BookListing;
+  export { BookListingForm };
+  
+
+  
   
